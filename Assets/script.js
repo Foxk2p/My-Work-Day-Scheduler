680bd2ffc0768fff60cf04a6c9a8ebefddca77b3
K2P <foxk2p@gmail.com>
2020-05-02 03:45:53 -0700

    function and format changes

---
+++ Assets/script.js
@@ -0,0 +1,109 @@
+// let timer = moment().format('LLLL');
+// console.log(timer)
+
+document.getElementById('btn9h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH9', document.getElementById('inputH9').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText9').innerHTML = `
+    <h6>${document.getElementById('inputH9').value}</h6>
+  `
+  document.getElementById('inputH9').value = ''
+})
+
+
+// if (localStorage.getItem('inputH9')) {
+//   document.getElementById('dispText9').textContent.innerHTML = `
+//     <h6>${document.getElementById('inputH9').value}</h6>
+//   `
+// }
+
+document.getElementById('btn10h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH10', document.getElementById('inputH10').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText10').innerHTML = `
+    <h6>${document.getElementById('inputH10').value}</h6>
+  `
+  document.getElementById('inputH10').value = ''
+})
+
+document.getElementById('btn11h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH11', document.getElementById('inputH11').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText11').innerHTML = `
+    <h6>${document.getElementById('inputH11').value}</h6>
+  `
+  document.getElementById('inputH11').value = ''
+})
+
+document.getElementById('btn12h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH12', document.getElementById('inputH12').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText12').innerHTML = `
+    <h6>${document.getElementById('inputH12').value}</h6>
+  `
+  document.getElementById('inputH12').value = ''
+})
+
+document.getElementById('btn13h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH13', document.getElementById('inputH13').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText13').innerHTML = `
+    <h6>${document.getElementById('inputH13').value}</h6>
+  `
+  document.getElementById('inputH13').value = ''
+})
+
+document.getElementById('btn14h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH14', document.getElementById('inputH14').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText14').innerHTML = `
+    <h6>${document.getElementById('inputH14').value}</h6>
+  `
+  document.getElementById('inputH14').value = ''
+})
+
+document.getElementById('btn15h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH15', document.getElementById('inputH15').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText15').innerHTML = `
+    <h6>${document.getElementById('inputH15').value}</h6>
+  `
+  document.getElementById('inputH15').value = ''
+})
+
+document.getElementById('btn16h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH16', document.getElementById('inputH16').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText16').innerHTML = `
+    <h6>${document.getElementById('inputH16').value}</h6>
+  `
+  document.getElementById('inputH16').value = ''
+})
+
+document.getElementById('btn17h').addEventListener('click', () => {
+  event.preventDefault()
+  localStorage.setItem('inputH17', document.getElementById('inputH17').value)
+  // console.log(document.getElementById('inputH9'))
+  document.getElementById('dispText17').innerHTML = `
+    <h6>${document.getElementById('inputH17').value}</h6>
+  `
+  document.getElementById('inputH17').value = ''
+})
+
+// document.getElementById('btn9h').addEventListener('click', () => {
+//   event.preventDefault()
+//   localStorage.setItem('ls9' document.getElementById('inputH9').value)
+//   console.log(document.getElementById('inputH9'))
+//   document.getElementById('dispText9').innerHTML = `
+//     <h6>${document.getElementById('inputH9').value}</h6>
+//   `
+//   document.getElementById('inputH9').value = ''
+// })
--- README.md
+++ README.md
@@ -7,9 +7,6 @@ Directions:
 Use the daily planner to create a schedule.
 The current day is displayed at the top of the calendar.
 Timeblocks are for standard business hours and you can view them for the day.
-Each timeblock is color coded to indicate whether it is in the past, present, or future.
 To add to the scheduler, enter text in the text field and click the timeblock button. 
-The text for that event is saved in local storage.
-When you refresh the page, the saved events persist.
 
 <img src="./Assets/My-Work-Day-Scheduler.png">
